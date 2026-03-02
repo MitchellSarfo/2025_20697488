b1f28faa89e1d8eb6ff3741f707036fa92da46b1
Mitchell <eeymo3@nottingham.ac.uk>
2026-03-02 18:56:04 +0000

    Worksheet 6 Ex2: button slots + QMessageBox

--- Worksheet6/Worksheet6_GUI/mainwindow.cpp
+++ Worksheet6/Worksheet6_GUI/mainwindow.cpp
@@ -1,14 +1,26 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-}
 
+    connect(ui->pushButton,   &QPushButton::released, this, &MainWindow::handleButton1);
+    connect(ui->pushButton_2, &QPushButton::released, this, &MainWindow::handleButton2);
+}
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+void MainWindow::handleButton1()
+{
+    QMessageBox::information(this, "Info", "Button 1 clicked");
+}
+
+void MainWindow::handleButton2()
+{
+    QMessageBox::information(this, "Info", "Button 2 clicked");
 }
