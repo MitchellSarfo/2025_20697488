bfb7f3966d0833003c4c0fa91259c6159a336a57
Mitchell Sarfo <Eeymo3@nottingham.ac.uk>
2026-04-27 07:50:11 +0100

    Worksheet 7 Exercise 2 - VTK OpenVR setup

--- Worksheet7/Exercise1 Build/cmake_install.cmake
+++ Worksheet7/Exercise1 Build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/2025_20697488/Worksheet7/Exercise1
+# Install script for directory: C:/Users/papas/2025_20697488/Worksheet7/Exercise1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/2025_20697488/Worksheet7/Exercise1 Build/install_local_manifest.txt"
+  file(WRITE "C:/Users/papas/2025_20697488/Worksheet7/Exercise1 Build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/2025_20697488/Worksheet7/Exercise1 Build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/papas/2025_20697488/Worksheet7/Exercise1 Build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
